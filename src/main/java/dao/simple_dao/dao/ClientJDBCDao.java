7b65164143867700e8e84d1df049bb537eed490b
Danila <dvfeduleev@gmail.com>
2021-02-02 00:20:50 +0300

    pattern DAO, add connection

--- src/main/java/dao/simple_dao/dao/ClientJDBCDao.java
+++ src/main/java/dao/simple_dao/dao/ClientJDBCDao.java
@@ -2,6 +2,9 @@ package dao.simple_dao.dao;
 
 import jdbc.carsshop.result_set.Client;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ClientJDBCDao implements ClientDAO {
@@ -29,4 +32,22 @@ public class ClientJDBCDao implements ClientDAO {
     public void remove(String name) {
 
     }
+
+    private Connection getConnection() {
+        Connection connection = null;
+
+        final String URL = "jdbc:mysql://localhost:3305/carsshop?useUnicode=true&serverTimezone=UTC&";
+        final String LOGIN = "root";
+        final String PASSWORD = "admin";
+
+        try {
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
 }
